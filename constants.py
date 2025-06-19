89434aff1908a184b5e4f5c501558396b9491cb8
Greg Emesih <159478071+GG413TTU@users.noreply.github.com>
2025-06-18 23:25:24 -0500

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,27 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 500
+HEIGHT = 500
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4 #For the cirles
+
+OFFSET = 50 #balance
+
+# --- COLORS ---
+#RGB Format
+BG_COLOR = (250, 213, 165) #Desert
+LINE_COLOR = (210, 125, 45) #Cinnamon
+CIRC_COLOR = (0, 0, 0) #Black
+CROSS_COLOR = (240, 128, 0) #Tangerine 
